6b2bca7c8ec6b169a5b66d40f310010ac6ae3364
Eduardo <lalorhp@gmail.com>
2019-11-08 13:16:58 -0500

    .

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/gutynatura'));
+app.use(express.static(__dirname+'/dist/negocios-electronicos'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/gutynatura/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/negocios-electronicos/index.html'));
 });
 
 app.listen(process.env.PORT || 8084);
